4c968c87931e1ac43a3717a0fc75249e98a17b89
ObamaBinModdin <52817466+ObamaBinModdin@users.noreply.github.com>
2023-03-06 21:56:54 -0800

    Update main.py
    Lower dpi and steps_per_period to decrease computation time. Will take 10 minutes as it is creating a 4k mp4.

--- main.py
+++ main.py
@@ -3,84 +3,67 @@ import pandas as pd
 import bar_chart_race as bcr
 from moviepy.editor import VideoFileClip, clips_array
 
-
 # Call and store inflation data into a dataframe.
-from matplotlib import pyplot as plt
-
 inflationDF = pd.read_excel('https://github.com/ObamaBinModdin/CS445-Final_Project/raw/main/inflation.xlsx')
-
-print('got here')
-
-# Replace all ".." values to "nan".
-inflationDF = inflationDF.replace('..', np.nan)
-
-# Change all year columns to type float.
-inflationDF.iloc[:, 4:] = inflationDF.iloc[:, 4:].astype(float)
-
-# Replace all "nan" values to the column mean ignoring outliers.
-for col in inflationDF.iloc[:, 4:].columns:
-    Q1 = inflationDF[col].quantile(0.25)
-    Q3 = inflationDF[col].quantile(0.75)
-    IQR = Q3 - Q1
-    df_filtered = inflationDF[(inflationDF[col] >= Q1 - 1.5 * IQR) & (inflationDF[col] <= Q3 + 1.5 * IQR)]
-    mean_col = df_filtered[col].mean()
-    inflationDF[col] = inflationDF[col].fillna(mean_col)
-
-inflationDF.drop(['Country Code', 'Series Code', 'Country Code', 'Series Name'], inplace=True, axis=1)
-
-inflationDF.set_index('Country Name', inplace=True)
-
-inflationDF.index.name = None
-
-inflationDF = inflationDF.T.reset_index().set_index('index')
-
-inflationDF.index.name = 'Year'
-
-
-# Call and store inflation data into a dataframe.
+# Call and store unemployment data into a dataframe.
 unemploymentDF = pd.read_excel('https://github.com/ObamaBinModdin/CS445-Final_Project/raw/main/unemployment_rate.xlsx')
 
-# Replace all ".." values to "nan".
-unemploymentDF = unemploymentDF.replace('..', np.nan)
 
-# Change all year columns to type float.
-unemploymentDF.iloc[:, 4:] = unemploymentDF.iloc[:, 4:].astype(float)
+def cleanData(df):
+    # Replace all ".." values to "nan".
+    df = df.replace('..', np.nan)
 
-# Replace all "nan" values to the column mean ignoring outliers.
-for col in unemploymentDF.iloc[:, 4:].columns:
-    Q1 = unemploymentDF[col].quantile(0.25)
-    Q3 = unemploymentDF[col].quantile(0.75)
-    IQR = Q3 - Q1
-    df_filtered = unemploymentDF[(unemploymentDF[col] >= Q1 - 1.5 * IQR) & (unemploymentDF[col] <= Q3 + 1.5 * IQR)]
-    mean_col = df_filtered[col].mean()
-    unemploymentDF[col] = unemploymentDF[col].fillna(mean_col)
+    # Change all year columns to type float.
+    df.iloc[:, 4:] = df.iloc[:, 4:].astype(float)
 
-unemploymentDF.drop(['Country Code', 'Series Code', 'Country Code', 'Series Name'], inplace=True, axis=1)
+    # Replace all "nan" values to the column mean ignoring outliers.
+    for col in df.iloc[:, 4:].columns:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        df_filtered = df[(df[col] >= Q1 - 1.5 * IQR) & (df[col] <= Q3 + 1.5 * IQR)]
+        mean_col = df_filtered[col].mean()
+        df[col] = df[col].fillna(mean_col)
 
-unemploymentDF.set_index('Country Name', inplace=True)
+    # Dropping useless columns.
+    df.drop(['Country Code', 'Series Code', 'Country Code', 'Series Name'], inplace=True, axis=1)
 
-unemploymentDF.index.name = None
+    # Setting column 'Country Name' to index.
+    df.set_index('Country Name', inplace=True)
 
-unemploymentDF = unemploymentDF.T.reset_index().set_index('index')
+    # Removing name for index column.
+    df.index.name = None
 
-unemploymentDF.index.name = 'Year'
+    # Transposing, resetting index, and setting index to 'index'.
+    df = df.T.reset_index().set_index('index')
+
+    # Rename index to year.
+    df.index.name = 'Year'
+
+    return df
 
 
-fig1 = bcr.bar_chart_race(df=inflationDF, title='My first bar chart race', steps_per_period=10,
-                                period_length=500, n_bars=10, interpolate_period=False,
-                                period_label={'x': .95, 'y': .5, 'ha': 'right', 'va': 'center'},
-                                period_fmt='Race 1: {x}', filename='test1.mp4')
+# Cleaning data for both datasets.
+inflationDF = cleanData(inflationDF)
+unemploymentDF = cleanData(unemploymentDF)
+
+fig1 = bcr.bar_chart_race(df=inflationDF.iloc[8:, :], title='Inflation, consumer prices (annual %)', dpi=600,
+                          steps_per_period=250, sort='desc', label_bars=True,
+                          period_length=3000, n_bars=6, interpolate_period=True, cmap='dark12',
+                          filter_column_colors=True,
+                          period_label={'x': .95, 'y': .5, 'ha': 'right', 'va': 'center'},
+                          period_fmt='{x:.0f}', filename='inflation_bcr.mp4', bar_kwargs={'alpha': 1})
 
 # Create the second bar chart race
-fig2 = bcr.bar_chart_race(df=unemploymentDF, n_bars=10, title='My second bar chart race', steps_per_period=10,
-                                period_length=500, interpolate_period=False,
-                                period_label={'x': .05, 'y': .5, 'ha': 'left', 'va': 'center'},
-                                period_fmt='Race 2: {x}', filename='test2.mp4')
+fig2 = bcr.bar_chart_race(df=unemploymentDF.iloc[8:, :], n_bars=6, title='Unemployment (annual %)', dpi=600,
+                          steps_per_period=250, sort='desc', label_bars=True,
+                          period_length=3000, interpolate_period=True, cmap='dark12', filter_column_colors=True,
+                          period_label={'x': .95, 'y': .5, 'ha': 'right', 'va': 'center'},
+                          period_fmt='{x:.0f}', filename='unemployment_bcr.mp4', bar_kwargs={'alpha': 1})
 
-print('got here 2.0')
-
-clip1 = VideoFileClip('test1.mp4')
-clip2 = VideoFileClip('test2.mp4')
+# mp4s of the bar chart races.
+clip1 = VideoFileClip('inflation_bcr.mp4')
+clip2 = VideoFileClip('unemployment_bcr.mp4')
 
 # Resize the videos to the same height
 clip1 = clip1.resize(height=360)
@@ -90,4 +73,4 @@ clip2 = clip2.resize(height=360)
 final_clip = clips_array([[clip1, clip2]])
 
 # Write the final clip to a new file
-final_clip.write_videofile('combined.mp4')
+final_clip.write_videofile('inflation_unemployment_bcr.mp4')
